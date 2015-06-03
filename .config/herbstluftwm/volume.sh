1d11b89ff1bd58c05ee4775b22e8351b3d272bab
BSE <bse666@gmail.com>
2015-06-03 19:57:28 +0200

    volume unmute fix for laptop

--- .config/herbstluftwm/volume.sh
+++ .config/herbstluftwm/volume.sh
@@ -3,14 +3,19 @@
 case $1 in
     down)
         amixer -q set Master 2%- unmute
+        amixer -q set Headphone unmute
+        amixer -q set Speaker unmute
         herbstclient emit_hook volume
         ;;
     up)
         amixer -q set Master 2%+ unmute
+        amixer -q set Headphone unmute
+        amixer -q set Speaker unmute
         herbstclient emit_hook volume
         ;;
     mute)
-        amixer -q set Master toggle
+        amixer -q set Master toggle 
+        #amixer -q set Headphone toggle;
         herbstclient emit_hook volume_toggle
         ;;
 esac
