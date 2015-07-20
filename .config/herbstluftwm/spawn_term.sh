9612ccfab658c2f355849c0f7b9de26039c323a3
BSE <bse666@gmail.com>
2015-07-20 11:09:43 +0200

    optimized start times

--- .config/herbstluftwm/spawn_term.sh
+++ .config/herbstluftwm/spawn_term.sh
@@ -9,7 +9,7 @@ function soll {
     xrdb -DSOLARIZED_LIGHT -merge $HOME/.Xdefaults
 }
 
-if [ $HOUR -lt "20" -a $HOUR -gt "10" ]
+if [ $HOUR -lt "20" -a $HOUR -gt "8" ]
 then
     soll
     urxvtc
@@ -17,7 +17,7 @@ elif [ $HOUR -gt "20" ]
 then
     sold 
     urxvtc
-elif [ $HOUR -gt "0" -a $HOUR -lt "10" ]
+elif [ $HOUR -gt "0" -a $HOUR -lt "8" ]
 then
     sold
     urxvtc
